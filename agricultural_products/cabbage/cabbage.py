213ebf81fd33829eff58726a9bbe4e5fb23d9cc1
youngjaeKim <hkm2379@gmail.com>
2023-08-21 17:37:24 +0900

    fix: [김영재] 파일 이름 수정

--- agricultural_products/cabbage/cabbage.py
+++ agricultural_products/cabbage/cabbage.py
@@ -1,13 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('양배추_09~19_가격.csv')
+df = pd.read_csv('양배추_09~19년.csv')
 print(df)
 
-#df = df.add_prefix('09/')
-#print(df)
-
-#df1 = df[['일자', '서울가락도매\n평균가격 (원/kg)']]
-#print(df1)
-
-#df1 = df1.fillna(0)
-#print(df1)
